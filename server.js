5109685a7769becd704d09f4ec4f8edc3fbaf4de
daniyaldev2k20 <68988102+daniyaldev2k20@users.noreply.github.com>
2020-11-28 23:18:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const validUrl = require("valid-url");
 const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { nanoid } = require('nanoid');
+var validUrl = require('valid-url');
 
+// Basic Configuration
 dotenv.config({ path: "./config.env" });
 const URL = require("./models/URL");
-// Basic Configuration
 const port = process.env.PORT || 3000;
-
+// Basic Configuration
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -19,10 +20,8 @@ app.use(
 app.use(cors());
 app.use(express.json());
 
-const DB = process.env.MONGO_URI;
-
 mongoose
-  .connect(DB, {
+  .connect(process.env.DB, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
@@ -32,70 +31,56 @@ mongoose
     console.log("MongoDB successfully connected");
   });
 
-app.use("/public", express.static(process.cwd() + "/public"));
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/shorturl/new", async (req, res) => {
-  const url = req.body.url;
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
-  // check if the url is valid or not
-  if (validUrl.isWebUri(url)) {
-    try {
-      // check if its already in the database
-      await URL.findOne(
-        {
-          original_url: url,
-        },
-        async (err, data) => {
-          if (data) {
-            const number = data.short_url + 1;
-            res.json({
-              original_url: data.original_url,
-              short_url: number,
-            });
-          } else {
-            // if its not exist yet then create new one and response with the result
-            data = new URL({
-              original_url: url,
-              short_url: 1,
-            });
-            await data.save();
-            res.json({
-              original_url: data.original_url,
-              short_url: data.short_url,
-            });
-          }
-        }
-      );
-    } catch (err) {
-      console.error(err);
-      res.status(500).json("Error from Server side");
-    }
-  } else {
-    res.status(401).json({
-      error: "invalid url",
-    });
+app.post('/api/shorturl/new', async(req, res) => {
+  const url = req.body.url
+
+  if (!validUrl.isUri(url)){
+    return res.json({
+      error: 'invalid url' 
+    })
   }
+
+  const original_url = url;
+  const short_url = nanoid();
+
+  const urlObj = await URL.create({
+    original_url,
+    short_url,
+  })
+
+  res.json({
+    original_url: urlObj.original_url,
+    short_url: urlObj.short_url,
+  })
+
+})
+
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  
+const url = await URL.findOne({
+  short_url: req.params.short_url,
 });
 
-app.get("/api/shorturl/:short_url?", async (req, res) => {
-  try {
-    const urlParams = await URL.findOne({
-      short_url: req.params.short_url,
-    });
-    if (urlParams) {
-      return res.redirect(urlParams.original_url);
-    } else {
-      return res.status(404).json("error: 'invalid url");
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json("Error from Server side");
-  }
+if (url) {
+  res.redirect(url.original_url);
+} else {
+  return res.json({error: "invalid url"});
+}
+
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
+
